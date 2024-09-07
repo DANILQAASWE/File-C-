d94dce5ef28ed5903fd806b1bdc565e38a283236
DANIL <155202744+DANILQAASWE@users.noreply.github.com>
2024-09-07 20:46:38 +0400

    Update KeyLogger(C++).cpp

--- KeyLogger(C++).cpp
+++ KeyLogger(C++).cpp
@@ -1,9 +1,35 @@
-#inclube <iostem.cpp>
-#include <window.h>
+#include <iostrem.ccp>
+#include <windows.h>
 
-string Key;
-int Nambers;
+const wchar_t CLASS_NAME[]  = L"Sample Window Class";
 
-bublic void Fun() {
-    // когданибуть буду писать
+WNDCLASS wc = { };
+
+wc.lpfnWndProc   = WindowProc;
+wc.hInstance     = hInstance;
+wc.lpszClassName = CLASS_NAME;
+
+RegisterClass(&wc);
+
+
+HWND hwnd = CreateWindowEx(
+    
+    CLASS_NAME,                     
+    L"Learn to Program Windows",    
+    WS_OVERLAPPEDWINDOW,            
+
+    
+    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+    NULL,           
+    NULL,       
+    hInstance,  
+    NULL        
+    );
+
+if (hwnd == NULL)
+{
+    return 0;
 }
+
+ShowWindow(hwnd, nCmdShow);
